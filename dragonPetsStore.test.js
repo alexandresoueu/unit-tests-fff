849aa0034ab9ba9473797455b737fea89f3dae8b
alexandresoueu <alexandresoueu@gmail.com>
2020-03-12 11:35:54 -0300

    config quokka and init unit test

---
+++ dragonPetsStore.test.js
@@ -0,0 +1,13 @@
+if(orderTotal({
+    items: [
+        { name: "Dragon food",         price: 8 },
+        { name: "Dragon cage (small)", price: 800 }
+    ]
+
+}) !== 808 ) {
+    throw new Error('Check fail: Happy path')
+}
+
+function orderTotal() {
+    return 808
+}
