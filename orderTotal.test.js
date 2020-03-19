7616712a1d69e2aa5425737c6e0934dac1e2dcd7
calexandredev <calexandredev@gmail.com>
2020-03-19 13:49:49 -0300

    create mock with jest

--- orderTotal.test.js
+++ orderTotal.test.js
@@ -1,54 +1,65 @@
 const orderTotal = require('./orderTotal')
 
-const emptyFunction = () => {}
+const emptyFunction = null
 
 it('calls vaptapi.com correctly', () => {
     let isFakeFetchCalled = false
-    
-    const fakeFetch = (url) => {
-        expect(url).toBe('https://vatapi.com/v1/country-code-check?code=DE')
-        isFakeFetchCalled = true
+    const fakeProcess = {
+      env: {
+        VAT_API_KEY: 'key123'
+      }
     }
-    orderTotal(fakeFetch, {
+    const fakeFetch = (url, opts) => {
+
+        expect(opts.headers.apikey).toBe('key123')
+        expect(url).toBe('https://vatapi.com/v1/country-code-check?code=DE')
+        return Promise.resolve({
+          json: () => Promise.resolve({
+            rates: {
+              standard:{
+                 value: 19
+              }
+            }
+          }) 
+        })
+    }
+    return orderTotal(fakeFetch, fakeProcess,{
         country: 'DE',
         items: [
             { 'name': 'Dragon Waffles', price: 20, quantity: 2 }
         ]
-    }).then(result => {
-        expect(isFakeFetchCalled).toBe(true)
-    })
+    }).then(result =>  expect(result).toBe(20*2*1.19))
 })
 
-it('if country code specified')
+it('Quantity',() =>
+  orderTotal(null, null,{
+      items:[
+          { name: 'Dragon candy', price: 2, quantity: 3}
+      ]
+  }).then(result => expect(result).toBe(6)))
 
-it('Quantity', () =>
-    orderTotal(emptyFunction, {
-        items: [
-            { 'name': 'Dragon candy',      price: 2, quantity: 3 }
-        ]
-    }).then(result => expect(result).toBe(6)))
+it('No quantity specified', () =>
+  orderTotal(null,null, {
+      items:[
+          {'name':'Dragon candy',price: 3}
+      ]
+  }).then(result => expect(result).toBe(3))
+)  
 
-it('No Quantity specified', () => 
-    orderTotal(emptyFunction, {
-        items: [
-            { 'name': 'Dragon candy',      price: 3 }
-        ]
-    }).then(result => expect(result).toBe(3)))
+it('Happy path (Example 1)', ()=>
+  orderTotal(null, null, {
+    items:[
+        { name:'Dragon food', price: 8, quantity: 1},
+        { name:'Dragon cage (small)', price: 800 , quantity:1}
+    ]    
+  }).then(result => expect(result).toBe(808))
+)
 
-it('Happy path Example One', () => 
-    orderTotal(emptyFunction, {
-        items: [
-            { name: "Dragon food",         price: 8,   quantity: 1 },
-            { name: "Dragon cage (small)", price: 800, quantity: 1 }
-        ]
-    
-    }).then(result => expect(result).toBe(808)))
-
-it('Happy path Example Two', () =>
-    orderTotal(emptyFunction, {
-        items: [
-            { name: "Dragon collar",         price: 20, quantity: 1 },
-            { name: "Dragon chew toy",       price: 40, quantity: 1 }
-        ]
-    
-    }).then(result => expect(result).toBe(60)))
+it('Happy path (Example 2)', () =>
+  orderTotal(null,null, {
+    items:[
+        { name:'Dragon collar', price: 20, quantity: 1 },
+        { name:'Dragon chew toy', price: 40, quantity: 1}
+    ]  
+  }).then(result => expect(result).toBe(60))
+)
---
+++ sandbox.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+const orderTotal = require('./orderTotal')
+
+const result = orderTotal(fetch, process, {
+  country: 'DE',
+  items: [
+    {'name': 'Dragon waffles', price: 20, quantity: 2 }
+  ]
+})
+result
+
+// const result =
+//     fetch('https://vatapi.com/v1/country-code-check?code=DE',{
+//         headers:{
+//           'apikey':process.env.VAT_API_KEY
+//         }
+//     })
+//     .then(response => response.Json())
+//     .then(data => data.rates.standart.value )
